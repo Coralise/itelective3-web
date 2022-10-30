8e7eb3d877a72c8502b37a725be5e872a1ec1a71
Corals <77341395+Coralise@users.noreply.github.com>
2022-10-30 22:45:58 +0800

    Commit 2245

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var fm = require('./firebase-manager');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  // Render the index page as well as pass the documents
+  res.render('index', { title: 'Express', url: req.url, posts: await fm.getPosts(), fm: fm });
 });
 
 module.exports = router;
